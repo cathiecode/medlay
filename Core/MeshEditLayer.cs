9c7ff0b8aa1800916d8186b49aeb71b39cc9bdb6
cathiecode <cathiecode@gmail.com>
2026-01-08 02:45:11 +0900

    change: MeshEditLayer Id is not globally unique

--- Core/MeshEditLayer.cs
+++ Core/MeshEditLayer.cs
@@ -3,6 +3,11 @@ namespace com.superneko.medlay.Core
     [System.Serializable]
     public abstract class MeshEditLayer
     {
+        /// <summary>
+        /// Id for this MeshEditLayer instance.
+        /// Please note that this Id is unique per each pipeline, not globally.
+        /// This restriction allows cloning/copying parent objects such as MonoBehaviours and ScriptableObjects without re-assigning Ids in Unity Editor.
+        /// </summary>
         public long Id { get; set; } = LongId.Generate();
     }
 }
