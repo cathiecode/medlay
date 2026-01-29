ac18d3845174b0a32a84265a64e878ee77e12ec3
cathiecode <cathiecode@gmail.com>
2026-01-30 00:15:08 +0900

    fix: default id serialization

--- Core/MeshEditLayer.cs
+++ Core/MeshEditLayer.cs
@@ -1,13 +1,21 @@
+using UnityEngine;
+
 namespace com.superneko.medlay.Core
 {
     [System.Serializable]
     public abstract class MeshEditLayer
     {
+        [SerializeField] private long _id = LongId.Generate();
+
         /// <summary>
         /// Id for this MeshEditLayer instance.
         /// Please note that this Id is unique per each pipeline, not globally.
         /// This restriction allows cloning/copying parent objects such as MonoBehaviours and ScriptableObjects without re-assigning Ids in Unity Editor.
         /// </summary>
-        public long Id { get; set; } = LongId.Generate();
+        public long Id
+        {
+            get => _id;
+            set => _id = value;
+        }
     }
 }
